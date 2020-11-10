12ff3be53a713c1a92adc792661543b2957a65fe
Gustav Larsson <gustav.larsson@gmail.com>
2020-11-10 11:29:49 +0100

    create node module

---
+++ index.js
@@ -0,0 +1,9 @@
+const UserSchema = require("./schemas/user");
+const OrganizationSchema = require("./schemas/organization");
+
+module.exports = {
+  schemas: {
+    OrganizationSchema,
+    UserSchema,
+  },
+};
